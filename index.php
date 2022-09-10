3a2888eadc3f6d4c4dd6f4772bbcb255eb1cbad3
Catalin Ciobanu <ciobanu.m.catalin@gmaill.com>
2022-09-10 12:19:01 +0300

    asdasd

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
 </head>
 <body>
 Hello world
+<?php
 session_start();
-<?php echo 'session cookie'; ?>
-<?php print_r($_SESSION); ?>
-<?php print_r($_COOKIE)?>
+echo 'session cookie';
+print_r($_SESSION);
+print_r($_COOKIE)
+?>
 </body>
 </html>
